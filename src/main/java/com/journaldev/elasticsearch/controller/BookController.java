7259b1d8bc6d772ade8a3e54161f0d28484a8be5
rohid dev <rohiddev@rohids-MacBook-Air.local>
2020-05-27 15:48:09 -0700

    u

--- src/main/java/com/journaldev/elasticsearch/controller/BookController.java
+++ src/main/java/com/journaldev/elasticsearch/controller/BookController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/books")
 public class BookController {
 
     private BookDao bookDao;
@@ -16,23 +15,38 @@ public class BookController {
         this.bookDao = bookDao;
     }
 
-    @PostMapping
+    @PostMapping("/books")
     public Book insertBook(@RequestBody Book book) throws Exception{
         return bookDao.insertBook(book);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/books/{id}")
     public Map<String, Object> getBookById(@PathVariable String id){
         return bookDao.getBookById(id);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/books/{id}")
     public Map<String, Object> updateBookById(@RequestBody Book book, @PathVariable String id){
         return bookDao.updateBookById(id, book);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/books/{id}")
     public void deleteBookById(@PathVariable String id){
          bookDao.deleteBookById(id);
     }
+    
+    @GetMapping("/hello")
+	public String greeting() {
+		return "Hello World 2";
+	}
+
+	@GetMapping("/")
+	public String greetingHello() {
+		
+		
+		
+		
+		return "Hello World";
+	}
+
 }
